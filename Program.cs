3bc28b66595338076d825ba2e644cdb864f2d7ae
Rafiq654 <62049468+Rafiq654@users.noreply.github.com>
2020-07-29 23:27:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            luasPersegiPanjang a = new luasPersegiPanjang();
+            KelilingPersegiPanjang b = new KelilingPersegiPanjang();
+            
+            a.Nilai(3 ,4);
+            b.Nilai(3, 4);
+            
+            Console.WriteLine("Luas Persegi Panjang = " + a.area());
+            Console.WriteLine("Keliling Persegi Panjang = " + b.area());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Values.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    class Values
+    {
+        protected int p, l;
+        public void Nilai(int a, int b)
+    {
+            p = a;
+            l = b;
+    }
+    }
+
+    class luasPersegiPanjang : Values {
+    public int area()
+    {
+    return (p * l);
+    }
+    }
+    class KelilingPersegiPanjang : Values {
+    public int area()
+    {
+    return ((p + l) * 2);
+    }
+    }
+}
